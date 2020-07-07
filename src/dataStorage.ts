3acd5934bf6b60bfaff46bec337421001cb3b347
nat13alia <natalia13krawczyk@gmail.com>
2020-07-07 17:20:51 +0200

    Form Creator Stage Two - Added DataStorage interface

---
+++ src/dataStorage.ts
@@ -0,0 +1,5 @@
+export interface DataStorage {
+  saveDocument(): void; //metoda powinna pobierać jako parametr obiekt z wartościami pól formularza (typ any) i zwracać identyfikator dokumentu (string)
+  loadDocument(): void // metoda powinna pobierać jako parametr identyfikator dokumentu i zwracać obiekt z wartościami dokumentu (czyli wartościami wypełnionych przez użytkownika pól formularza)
+  getDocuments(): void // metoda powinna zwracać tablicę idenfikatorów wszystkich zapisanych dokumentów.
+}
